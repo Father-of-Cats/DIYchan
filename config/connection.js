10aebcb04f800ca7fb1791df6d5597c40453486b
ElmerPC <erey2790@gmail.com>
2022-02-24 19:46:31 -0600

    added a few routes for User

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
-
-require('dotenv').config();
-
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-  
-  module.exports = sequelize;
-=======
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -28,4 +13,3 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
->>>>>>> feature/database
