a8ce5529791a5e42e3c09d839c938a1483b51325
ElmerPC <erey2790@gmail.com>
2022-03-01 18:52:00 -0600

    commented out the server const temporary

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
