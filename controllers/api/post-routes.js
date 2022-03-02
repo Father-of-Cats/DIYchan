886c7f75aac319bbff25cdc555c8219bc317b38d
ElmerPC <erey2790@gmail.com>
2022-03-01 18:43:44 -0600

    added comments routes create comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,19 +1,48 @@
 const router = require('express').Router();
-const Post  = require('../../models/Posts');
+const { Posts, Comments } = require('../../models/');
 
 router.get('/post/:id', (req, res) => {
-    const post = {
-      id: 1,
-      post_url: 'https://handlebarsjs.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      comments: [{}, {}],
-      user_id: {
-        users: 'test_user'
-      }
-    };
+    Posts.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'user_id',
+        'sub_id',
+        'post_data',
+        'created_at'
+       
+      ],
+      include: [
+        {
+          model: Comments,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
   
-    res.render('single-post', { post });
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        // res.render('single-post', { post });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
-
 module.exports = router;
