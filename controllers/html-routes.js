e7084edcd8e97bde21217377a68d8ceacd39a9bb
Jorge A Salinas Jr <asalinas212@outlook.com>
2022-03-03 14:48:25 -0600

    added html-routes, updated index.js to include html-routes, moved html from homepage to main to fix default view

---
+++ controllers/html-routes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { Subchans, Users } = require("../models");
+
+// get all subs for homepage
+router.get("/", (req, res) => {
+    res.render("homepage");
+    // Subchans.findAll({
+    //     include: [Users],
+    // })
+    //     .then((dbPostData) => {
+    //         const subs = dbPostData.map((id) => id.get({ plain: true }));
+
+    //         res.render("login", { subs });
+    //     })
+    //     .catch((err) => {
+    //         res.status(500).json(err);
+    //     });
+});
+
+router.get("/login", (req, res) => {
+    // if (req.session.loggedIn) {
+    //     res.redirect("/");
+    //     return;
+    // }
+
+    res.render("login");
+});
+
+module.exports = router;
